9d22e37f88dfd59cbd94135ca9f73a72b8e3d608
JoaoPedr0Maciel <joaopedromaciel007@gmail.com>
2025-10-22 19:09:41 -0400

    port env

--- src/main.ts
+++ src/main.ts
@@ -46,7 +46,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT;
   await app.listen(port);
 
   console.log(`Application is running on: http://localhost:${port}`);
