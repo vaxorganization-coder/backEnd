391ddb4b54e9a7dd50f5d2f08305f9ffbe52c77a
JoaoPedr0Maciel <joaopedromaciel007@gmail.com>
2025-10-22 20:20:11 -0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,11 @@ async function bootstrap() {
   
   app.enableCors({
     origin,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With'],
+    credentials: true,
   });
-  
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
